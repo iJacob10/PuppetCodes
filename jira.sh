e0f98f8e78ab9b7dbf082315b4fc6ae755ea239a
iJacob10 <06iylinjacob@gmail.com>
2017-04-26 04:14:32 -0700

    JIRA Expect Puppet(inside files)- Iylin

--- jira.sh
+++ jira.sh
@@ -1,34 +1,26 @@
-#!/bin/bash
-expect -c"
-spawn /opt/software/jira/atlassian-jira-software-7.2.3-x64.bin
+#!/usr/bin/expect -f
+spawn "/opt/software/jira/atlassian-jira-software-7.2.3-x64.bin"
 
-expect \"OK [o, Enter], Cancel [c]\"
-send \"\r\"
+expect "This will install JIRA Software 7.2.3 on your computer."
+send "o\r"
+expect "Choose the appropriate installation or upgrade option."
+send "2\r"
+expect "Where should JIRA Software be installed?"
+send "\r"
+expect "Default location for JIRA Software data"
+send "\r"
+expect "Configure which ports JIRA Software will use."
+send "2\r"
+expect "HTTP Port Number"
+send "8091\r"
+expect "Control Port Number"
+send "8007\r"
+expect "JIRA can be run in the background"
+send "y\r"
+expect "Details on where JIRA Software will be installed and the settings that will be used."
+send "i\r"
+expect "Please wait a few moments while JIRA Software is configured."
+send "y\r"
+expect "Finishing installation ..."
+send "exit\r"
 
-expect \"Express Install (use default settings) [1], Custom Install (recommended for advanced users) [2, Enter], Upgrade an existing JIRA installation [3]\"
-send \"2\r\"
-
-expect \"[/opt/atlassian/jira]\"
-send \"\r\"
-
-expect \"[/var/atlassian/application-data/jira]\"
-send \"\r\"
-
-expect \"Use default ports (HTTP: 8080, Control: 8005) - Recommended [1, Enter], Set custom value for HTTP and Control ports [2]\"
-send \"2\r\"
-
-expect \"[8080]\r\"
-send \"8091\r\"
-
-expect \"[8005]\r\"
-send \"8006\r\"
-
-expect \"Yes [y, Enter], No [n]\"
-send \"Yes\r\"
-
-expect \"Install [i, Enter], Exit [e]\"
-send \"i\r\"
-
-expect \"Yes [y, Enter], No [n]\"
-send \"Yes\r\"
-"
