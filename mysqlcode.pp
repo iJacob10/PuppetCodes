6222a2f0c751819e33010c5d124182ef1a4430c8
iJacob10 <06iylinjacob@gmail.com>
2017-04-26 04:13:52 -0700

    MySQL Puppet(inside modules)- Iylin

--- mysqlcode.pp
+++ mysqlcode.pp
@@ -4,6 +4,23 @@ $bin="mysql-community-client-5.6.35-2.el7.x86_64.rpm"
 $bin2="mysql-community-common-5.6.35-2.el7.x86_64.rpm"
 $bin3="mysql-community-libs-5.6.35-2.el7.x86_64.rpm"
 $bin4="mysql-community-server-5.6.35-2.el7.x86_64.rpm"
+/*file {"/tmp/mysql.tar.gz":
+ensure => 'file',
+source =>'puppet:///modules/mysql/mysql.tar.gz',
+}*/
+ 
+/**
+	exec {"remove-postfix":
+		path => ["/usr/sbin/","/usr/bin/"],
+		command => "rpm -e postfix-2.10.1-6.el7.x86_64",
+		}
+
+
+	exec {"remove-maria":
+		path => ["/usr/sbin/","/usr/bin/"],
+		command => "rpm -e mariadb-libs-5.5.44-2.el7.centos.x86_64",
+		}
+**/
 
 	        file { '/opt/software/mysql/':
                         ensure => 'directory',
@@ -43,6 +60,22 @@ $bin4="mysql-community-server-5.6.35-2.el7.x86_64.rpm"
                         path=>['/usr/bin/','/usr/sbin/',],
         }
 
+
+
+/*exec {"untar":
+require => File["/tmp/mysql.tar.gz"],
+path => ["/usr/sbin/","/usr/bin/"],
+cwd =>'/tmp/',
+command => "tar -xzf mysql.tar.gz",
+}*/
+
+/*exec {"installing":
+require => Exec["untar"],
+path => ["/usr/sbin/","/usr/bin/"],
+cwd => '/tmp/mysql/',
+command => "yum localinstall -y nogpgcheck *.rpm",
+}*/
+
 service {'mysqld':
 require => Exec["installing"],
 start => "service mysqld start",
@@ -58,7 +91,7 @@ mode => '0755',
 
 exec {"script":
 require => File["/tmp/my.sh"],
-path => ["/usr/bin/","/usr/sbin/","/bin/bash/","/bin/","/usr/local/bin/"],
+path => ["/usr/bin", "/usr/sbin/" , "/bin/bash/", "/bin/","/usr/local/bin/"],
 command => "/tmp/my.sh",
 }
 
